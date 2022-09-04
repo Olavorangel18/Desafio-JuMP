7866e405f209a675156460fdd15c6d6de17883b3
Olavo Conceicao <orc@cin.ufpe.br>
2022-09-03 21:04:46 -0300

    table: criacao de componente table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProcessosTableComponent } from './pages/processos-table/processos-table.component';
 import { HeaderComponent } from './components/header/header.component';
 import { IdentificacaoPaginaComponent } from './components/identificacao-pagina/identificacao-pagina.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProcessosTableComponent,
     HeaderComponent,
-    IdentificacaoPaginaComponent
+    IdentificacaoPaginaComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
